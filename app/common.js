5d7528d0b6efe69931e2cc9532791b28c0d9fb05
KAMLESH HARGUN <kamleshhargun@gmail.com>
2026-04-11 18:52:39 +0530

    Update common.js

--- app/common.js
+++ app/common.js
@@ -391,7 +391,7 @@ else if (page === "finished") {
 
   loadLabours();
   renderFinishedTable();
-
+}
 /* ===== FINISHED DATA ===== */
 let finishedEditIndex = null;
 
@@ -460,7 +460,7 @@ function loadPending() {
     }
   }
 }
-}
+
  /* ================= SALES ================= */
 else if (page === "sales") {
 
