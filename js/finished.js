f3842dfd28c2843d29ab44c1d3dffa081ce3a95a
KAMLESH HARGUN <kamleshhargun@gmail.com>
2026-04-12 13:20:06 +0530

    Update finished.js

--- js/finished.js
+++ js/finished.js
@@ -1,1 +1,10 @@
+function renderFinishedPage(){
+let c=document.getElementById("content");
+c.innerHTML=`     <h2>Finished</h2>     <button onclick="saveFinished()">Test</button>
+  `;
+}
 
+function saveFinished(){
+finishedData.push({test:1});
+localStorage.setItem("finishedData",JSON.stringify(finishedData));
+}
