b3a8cfc3af2935a8cf930a73024fa01bf13a80d9
KAMLESH HARGUN <kamleshhargun@gmail.com>
2026-04-12 13:19:41 +0530

    Update cutting.js

--- js/cutting.js
+++ js/cutting.js
@@ -1,1 +1,10 @@
+function renderCuttingPage(){
+let c=document.getElementById("content");
+c.innerHTML=`     <h2>Cutting</h2>     <button onclick="saveCutting()">Test</button>
+  `;
+}
 
+function saveCutting(){
+cuttingData.push({test:1});
+localStorage.setItem("cuttingData",JSON.stringify(cuttingData));
+}
