ea19d5d30b9e574378e39810dabaea6d9238c69c
KAMLESH HARGUN <kamleshhargun@gmail.com>
2026-04-12 13:20:21 +0530

    Update sales.js

--- js/sales.js
+++ js/sales.js
@@ -1,1 +1,10 @@
+function renderSalesPage(){
+let c=document.getElementById("content");
+c.innerHTML=`     <h2>Sales</h2>     <button onclick="saveSale()">Test</button>
+  `;
+}
 
+function saveSale(){
+salesData.push({test:1});
+localStorage.setItem("salesData",JSON.stringify(salesData));
+}
