eadc6c1c64e515437724463030387645ca96b2dd
KAMLESH HARGUN <kamleshhargun@gmail.com>
2026-04-12 13:20:50 +0530

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,16 @@
+function clearAll(){
+if(confirm("Delete all?")){
+localStorage.clear();
+location.reload();
+}
+}
 
+function setupKeyboard(){
+document.addEventListener("keydown",e=>{
+if(e.altKey){
+let pages=["dashboard","fabric","cutting","stitching","finished","sales","return","settings"];
+let i=parseInt(e.key)-1;
+if(pages[i]) loadPage(pages[i]);
+}
+});
+}
