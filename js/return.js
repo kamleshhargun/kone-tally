95db36103ceb61af5aec8828a249e9fca085f7a1
KAMLESH HARGUN <kamleshhargun@gmail.com>
2026-04-12 13:20:34 +0530

    Update return.js

--- js/return.js
+++ js/return.js
@@ -1,1 +1,10 @@
+function renderReturnPage(){
+let c=document.getElementById("content");
+c.innerHTML=`     <h2>Return</h2>     <button onclick="saveReturn()">Test</button>
+  `;
+}
 
+function saveReturn(){
+returnData.push({test:1});
+localStorage.setItem("returnData",JSON.stringify(returnData));
+}
