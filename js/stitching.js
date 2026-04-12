dc0f20ffe88157bcd1642d5ea76e2688ce4b3d52
KAMLESH HARGUN <kamleshhargun@gmail.com>
2026-04-12 13:19:53 +0530

    Update stitching.js

--- js/stitching.js
+++ js/stitching.js
@@ -1,1 +1,10 @@
+function renderStitchPage(){
+let c=document.getElementById("content");
+c.innerHTML=`     <h2>Stitching</h2>     <button onclick="saveStitching()">Test</button>
+  `;
+}
 
+function saveStitching(){
+stitchingData.push({test:1});
+localStorage.setItem("stitchingData",JSON.stringify(stitchingData));
+}
